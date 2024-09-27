96ed7e7f052faed267a97cfc0367937f4eaa10e1
bapibiswas100808 <star.bappy@gmail.com>
2024-09-27 12:46:25 +0600

    farmer and product category done

--- src/app/Pages/Homepage/FarmerSpotlight/FarmerSpotlight.jsx
+++ src/app/Pages/Homepage/FarmerSpotlight/FarmerSpotlight.jsx
@@ -6,13 +6,13 @@ import hero4 from "../../../asset/Image4.jpg";
 import hero6 from "../../../asset/Image6.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import "swiper/css/navigation"; // Import navigation styles
+import { Navigation } from "swiper/modules"; // Import the Navigation module
 import Image from "next/image";
 
 const FarmerSpotlight = () => {
   return (
-    <div className="max-w-[1170px] mx-auto mt-10 lg:mt-14 px-3 lg:px-0">
+    <div className="max-w-[1170px] mx-auto mt-10 lg:mt-14 px-3 lg:px-0 mb-10">
       {/* Heading */}
       <div className="text-center">
         <h2 className="text-2xl lg:text-3xl font-bold text-center py-2 text-yellow bg-green-600 inline-block px-5 mb-5 lg:mb-10 rounded-lg">
@@ -36,12 +36,16 @@ const FarmerSpotlight = () => {
       </div>
 
       {/* Slider */}
-      <div className="mt-10">
+      <div className="relative mt-10">
+        {/* Navigation buttons */}
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+
         <Swiper
-          slidesPerView={1}
           spaceBetween={20}
-          pagination={{
-            clickable: true,
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
           }}
           breakpoints={{
             640: {
@@ -57,48 +61,132 @@ const FarmerSpotlight = () => {
               spaceBetween: 30,
             },
           }}
-          modules={[Pagination]}
+          modules={[Navigation]} // Use Navigation module
           className="mySwiper"
         >
+          {/* Slide 1 */}
           <SwiperSlide>
-            <Image
-              className="min-h-[300px]"
-              src={hero1}
-              alt="hero"
-              height={500}
-            />
+            <div className="card bg-base-100 h-[550px]">
+              <figure>
+                <Image
+                  className="min-h-[300px]"
+                  src={hero1}
+                  alt="hero"
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Rahim Mondal: Organic Farming Pioneer
+                </h2>
+                <p>
+                  Rahim has been a passionate advocate for organic farming for
+                  over 15 years, ensuring that his crops are grown without
+                  chemicals and pesticides. His farm supplies some of the
+                  freshest and most nutritious produce in the region.
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
+
+          {/* Slide 2 */}
           <SwiperSlide>
-            <Image
-              className="min-h-[300px]"
-              src={hero2}
-              alt="hero"
-              height={500}
-            />
+            <div className="card bg-base-100 h-[550px]">
+              <figure>
+                <Image
+                  className="min-h-[300px]"
+                  src={hero2}
+                  alt="hero"
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Karim Gain: Champion of Sustainability
+                </h2>
+                <p>
+                  Karim has made it her mission to implement sustainable farming
+                  practices that protect the environment. Her dedication to
+                  water conservation and soil health is making a real difference
+                  in the farming community.
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
+
+          {/* Slide 3 */}
           <SwiperSlide>
-            <Image
-              className="min-h-[300px]"
-              src={hero3}
-              alt="hero"
-              height={500}
-            />
+            <div className="card bg-base-100 h-[550px]">
+              <figure>
+                <Image
+                  className="min-h-[300px]"
+                  src={hero3}
+                  alt="hero"
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Monir Mia: Innovator in Urban Agriculture
+                </h2>
+                <p>
+                  Monir Mia is a pioneer in farming and he is at the forefront
+                  of urban agriculture, turning city spaces into thriving farms.
+                  His innovative techniques help urban dwellers access fresh,
+                  locally grown produce right in the heart of the city.
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
+
+          {/* Slide 4 */}
           <SwiperSlide>
-            <Image
-              className="min-h-[300px]"
-              src={hero4}
-              alt="hero"
-              height={500}
-            />
+            <div className="card bg-base-100 h-[550px]">
+              <figure>
+                <Image
+                  className="min-h-[300px]"
+                  src={hero4}
+                  alt="hero"
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Ranjit Das: Guardian of Heritage Seeds
+                </h2>
+                <p>
+                  Ranjit Das is a professional farmer and he is dedicated to
+                  preserving heritage seed varieties, growing heirloom crops
+                  that have been passed down through generations. Her farm is a
+                  living history of agricultural biodiversity.
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
+
+          {/* Slide 5 */}
           <SwiperSlide>
-            <Image
-              className="min-h-[300px]"
-              src={hero6}
-              alt="hero"
-              height={500}
-            />
+            <div className="card bg-base-100 h-[550px]">
+              <figure>
+                <Image
+                  className="min-h-[300px]"
+                  src={hero6}
+                  alt="hero"
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  Amena Begum: Advocate for Farm-to-Table
+                </h2>
+                <p>
+                  Amena works tirelessly to connect local farmers with
+                  restaurants and consumers, promoting the farm-to-table
+                  movement. His passion for fresh, locally grown food is helping
+                  strengthen the bond between growers and the community.
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
--- src/app/Pages/Homepage/ProductCategories/ProductCategories.jsx
+++ src/app/Pages/Homepage/ProductCategories/ProductCategories.jsx
@@ -1,7 +1,279 @@
+import Image from "next/image";
 import React from "react";
 
 const ProductCategories = () => {
-  return <div>this is product categories</div>;
+  const Categories = [
+    {
+      id: 1,
+      name: "Grains",
+      imageUrl: "https://i.ibb.co/HFqNfdP/grains.jpg",
+      subcategories: [
+        {
+          id: 101,
+          name: "Rice",
+          types: ["Basmati", "Aman", "Boro", "IRRI", "Miniket"],
+        },
+        {
+          id: 102,
+          name: "Wheat",
+          types: ["Local Wheat", "Hybrid Wheat"],
+        },
+        {
+          id: 103,
+          name: "Maize",
+          types: ["Yellow Maize", "White Maize"],
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Fibers",
+      imageUrl: "https://i.ibb.co/ZVPcH5Z/fibers.jpg",
+      subcategories: [
+        {
+          id: 201,
+          name: "Jute",
+          types: ["Tossa Jute", "White Jute", "Mesta"],
+        },
+        {
+          id: 202,
+          name: "Cotton",
+          types: ["Upland Cotton", "Desi Cotton"],
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Fruits",
+      imageUrl: "https://i.ibb.co/FhKh4LW/fruits.jpg",
+      subcategories: [
+        {
+          id: 301,
+          name: "Mango",
+          types: ["Himsagar", "Langra", "Fazli", "Gopalbhog"],
+        },
+        {
+          id: 302,
+          name: "Banana",
+          types: ["Sabri", "Amritsagar", "Kacha Kola"],
+        },
+        {
+          id: 303,
+          name: "Jackfruit",
+          types: ["Echors", "Kanthal"],
+        },
+        {
+          id: 304,
+          name: "Guava",
+          types: ["Local Guava", "Hybrid Guava"],
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Vegetables",
+      imageUrl: "https://i.ibb.co/k5RfVqM/vegetables.jpg",
+      subcategories: [
+        {
+          id: 401,
+          name: "Potato",
+          types: ["Diamond", "Granola", "Cardinal"],
+        },
+        {
+          id: 402,
+          name: "Brinjal",
+          types: ["Round Brinjal", "Long Brinjal"],
+        },
+        {
+          id: 403,
+          name: "Tomato",
+          types: ["Roma", "Cherry Tomato"],
+        },
+        {
+          id: 404,
+          name: "Cabbage",
+          types: ["Green Cabbage", "Red Cabbage"],
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Fish",
+      imageUrl: "https://i.ibb.co/JzbxFRC/fish.jpg",
+      subcategories: [
+        {
+          id: 501,
+          name: "Freshwater Fish",
+          types: ["Rohu", "Catla", "Tilapia", "Pangasius"],
+        },
+        {
+          id: 502,
+          name: "Hilsa",
+          types: ["River Hilsa", "Sea Hilsa"],
+        },
+        {
+          id: 503,
+          name: "Shrimp",
+          types: ["Bagda", "Galda"],
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: "Spices",
+      imageUrl: "https://i.ibb.co/jH8mM1M/spices.jpg",
+      subcategories: [
+        {
+          id: 601,
+          name: "Chili",
+          types: ["Green Chili", "Dry Red Chili"],
+        },
+        {
+          id: 602,
+          name: "Turmeric",
+          types: ["Local Turmeric", "Hybrid Turmeric"],
+        },
+        {
+          id: 603,
+          name: "Garlic",
+          types: ["Local Garlic", "Imported Garlic"],
+        },
+        {
+          id: 604,
+          name: "Ginger",
+          types: ["Local Ginger", "Imported Ginger"],
+        },
+      ],
+    },
+    {
+      id: 7,
+      name: "Pulses",
+      imageUrl: "https://i.ibb.co/TBcct5K/pulses.jpg",
+      subcategories: [
+        {
+          id: 701,
+          name: "Lentils",
+          types: ["Red Lentil", "Yellow Lentil"],
+        },
+        {
+          id: 702,
+          name: "Chickpeas",
+          types: ["Kabuli Chickpeas", "Desi Chickpeas"],
+        },
+        {
+          id: 703,
+          name: "Green Gram",
+          types: ["Local Green Gram", "Hybrid Green Gram"],
+        },
+      ],
+    },
+    {
+      id: 8,
+      name: "Dairy Products",
+      imageUrl: "https://i.ibb.co/sbMsjJN/dairy.jpg",
+      subcategories: [
+        {
+          id: 801,
+          name: "Milk",
+          types: ["Cow Milk", "Buffalo Milk"],
+        },
+        {
+          id: 802,
+          name: "Cheese",
+          types: ["Paneer", "Chhana"],
+        },
+        {
+          id: 803,
+          name: "Yogurt",
+          types: ["Sweet Yogurt", "Plain Yogurt"],
+        },
+      ],
+    },
+    {
+      id: 9,
+      name: "Meat",
+      imageUrl: "https://i.ibb.co/gtSS4jS/meats.jpg",
+      subcategories: [
+        {
+          id: 901,
+          name: "Beef",
+          types: ["Local Beef", "Imported Beef"],
+        },
+        {
+          id: 902,
+          name: "Chicken",
+          types: ["Broiler Chicken", "Desi Chicken"],
+        },
+        {
+          id: 903,
+          name: "Goat Meat",
+          types: ["Local Goat Meat", "Imported Goat Meat"],
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="max-w-[1170px] mx-auto mt-10 lg:mt-14 px-3 lg:px-0 mb-10">
+      {/* Heading */}
+      <div className="text-center">
+        <h2 className="text-2xl lg:text-3xl font-bold text-center py-2 text-yellow bg-green-600 inline-block px-5 mb-5 lg:mb-10 rounded-lg">
+          Product Categories
+        </h2>
+      </div>
+
+      {/* Sub heading */}
+      <div className="text-center">
+        <h2 className="text-2xl lg:text-3xl mb-3 font-bold text-green">
+          Discover Our Agricultural Treasures:
+        </h2>
+        <p>
+          Explore a wide range of agricultural products sourced from the rich
+          lands of Bangladesh. From fresh grains and vibrant fruits to
+          sustainable fibers and nutritious fish, our categories highlight the
+          diverse products that contribute to a healthy, sustainable lifestyle.
+          Learn about each category and the importance they hold in our farming
+          communities.
+        </p>
+      </div>
+
+      {/* Products */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 mt-10">
+        {Categories.map((category) => (
+          <div key={category?.id} className="bg-base-100 rounded-lg">
+            <div className="">
+              <Image
+                src={category.imageUrl}
+                width={500}
+                height={800}
+                alt={category?.name}
+                className="w-full rounded-lg"
+              />
+            </div>
+            <div className="p-3">
+              <h2 className="text-green font-bold text-2xl mb-2">
+                {category?.name}
+              </h2>
+              <ul className="list-disc ml-5">
+                {category?.subcategories?.map((sub) => (
+                  <li className="" key={sub.id}>
+                    <span className="font-bold text-md">{sub.name}:</span>
+                    <p className="">
+                      {sub?.types?.map((type, idx) => (
+                        <span className="" key={idx}>
+                          {type},
+                        </span>
+                      ))}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductCategories;
